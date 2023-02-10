d6b702681baef1f1dc10f9e78510ed3369494362
ishanrai13 <ishanbansal012@gmail.com>
2023-02-10 12:19:34 +0530

    Roost Test Commit

---
+++ RoostTest/test-cypress/cypress/test-cypress.spec.js
@@ -0,0 +1,17 @@
+
+
+This is an example of a Cypress test:
+
+describe('My First Test', () => {
+  it('Visits the Kitchen Sink', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    cy.url().should('include', '/commands/actions')
+
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com')
+  })
+})
